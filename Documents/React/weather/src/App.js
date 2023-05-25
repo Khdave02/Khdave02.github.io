d175b44b84cb435a768a3ca414b2d75f4bec8051
unknown <khushidave2002@gmail.com>
2023-05-25 16:32:57 +0530

    pushing files

---
+++ Documents/React/weather/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import "./components/style.css"
+import Temp from "./components/temp"
+function App() {
+  return (
+    <div className="App">
+      <Temp />
+    </div>
+  );
+}
+
+export default App;
